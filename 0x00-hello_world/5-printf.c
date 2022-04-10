a11ee7c4f29d3d4bf9ef28f6fd6160a99857b0a4
Lyriqq <hannm927@gmail.com>
2022-04-10 15:40:21 -0700

    Task 4, 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,8 +6,6 @@
 * Return: Function ends
 */
 int main(void)
-{
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+{ printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
-
