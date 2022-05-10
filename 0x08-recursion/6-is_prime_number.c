79fa38d435fdcf1e8ce78e3365dc2f48302f2183
Lyriqqq <hannm927@gmail.com>
2022-05-10 22:28:55 +0300

    C Recursions Task 4,5,6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+
+#include "main.h"
+
+int prime(int m, int i);
+
+/**
+  * is_prime_number - returns 1 if the input is a prime number, otherwise 0
+  * @n: number
+  * Return: 1 or 0
+  **/
+
+
+int is_prime_number(int n)
+{
+	int x = 2;
+
+	return (prime(n, x));
+}
+
+/**
+  * prime - Function that calclate if a number is prime
+  * @m: number
+  * @i: root
+  * Return: 1 or 0
+  **/
+
+int prime(int m, int i)
+{
+	if (m % i == 0 || m < 2)
+	{
+		return (0);
+	}
+	if (m - 1 == i)
+	{
+		return (1);
+	}
+	else if (m > i)
+	{
+		return (prime(m, ++i));
+	}
+	return (1);
+}
