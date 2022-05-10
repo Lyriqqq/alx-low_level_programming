9744480ebd44cb46fb0cf37293bff34196db9b2f
Lyriqqq <hannm927@gmail.com>
2022-05-11 02:45:06 +0300

    Task 0,1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that  prints an argument passed to it, followed by a new line
+ * @argc: x args
+ * @argv: array  args
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
+
