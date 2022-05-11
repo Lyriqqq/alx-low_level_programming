beab7c91302fb5b00b12d9a8eee33de864cf3730
Lyriqqq <hannm927@gmail.com>
2022-05-11 03:23:17 +0300

    Fix task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *main - program that prints the minimum number of coins
- to make change for an amount of money
+ * to make change for an amount of money
  *
  *@argc: counter
  *@argv: vector
