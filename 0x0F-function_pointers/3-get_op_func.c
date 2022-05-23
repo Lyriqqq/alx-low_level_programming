21abb25fdb23613a28d1815410a94b6215687ca1
Lyriqqq <hannm927@gmail.com>
2022-05-24 01:06:56 +0300

    Rask 3 betty fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,7 +6,8 @@
   * get_op_func - function that selects the correct function
   * to perform the operation asked by the user
   * @s: the operator passed as argument to the program
-  * Return: a pointer to the function that corresponds to the operator given as a parameter.
+  * Return: a pointer to the function that
+  * corresponds to the operator given as a parameter.
   **/
 
 int (*get_op_func(char *s))(int, int)
