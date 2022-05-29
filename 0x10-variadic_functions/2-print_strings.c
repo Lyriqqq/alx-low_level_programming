209982b7bc84ea692c1d17cf282ee6e7ab0230ec
Lyriqqq <hannm927@gmail.com>
2022-05-29 17:40:19 +0300

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+  * print_strings - function that prints strings, followed by a new line
+  * @n:number of strings passed to the function
+  * @separator: string to be printed between numbers
+  * Return: Void
+  **/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *p;
+	va_list lst; /* creating the list */
+
+	/* initialize the list to store all values after n */
+	va_start(lst, n);
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(lst, char*);
+		if (p != NULL) /* treat arguments like strings */
+			printf("%s", p);
+		else
+			printf("(nil)");
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(lst); /* cleaning up the list */
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_all - function that prints anything
+  * @format: is a list of types of arguments passed to the function
+  * Return: Void
+  **/
+
+void print_all(const char * const format, ...)
+{
+	char *s;
+	int l = 0;
+	va_list lst; /* creating the list */
+
+	va_start(lst, format);
+	while (format == NULL)
+	{	printf("\n");
+		return;
+	}
+	while (format[l])
+	{
+		switch (format[l])
+		{
+			case 'c':
+				printf("%c", va_arg(lst, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(lst, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(lst, double));
+				break;
+			case 's':
+				s = va_arg(lst, char*);
+				if (s == NULL)
+				{	printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+		}
+		if ((format[l] == 'c' || format[l] == 'i' || format[l] == 'f' ||
+					format[l] == 's') && format[l + 1] != '\0')
+			printf(", ");
+		l++;
+	}
+	va_end(lst); /* cleaning up the list */
+	printf("\n");
+}
